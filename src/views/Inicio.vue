a727d3d99e709fa3321cf738d98787a3bd035bc3
Barby87 <bcaceres.sepulveda@hotmail.com>
2020-05-31 19:54:33 -0400

    changes on Inicio view

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -12,17 +12,13 @@
             </div>
         </header>
         <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-lg-8">
-                    <div class="post-preview">
-                        <router-link to="/post/1">
-                            <h2 class="post-title"><strong>Chevrolet presenta la totalmente nueva Blazer</strong></h2>
-                            <h3 class="post-subtitle">Llega&nbsp;como la primera SUV deportiva de la marca</h3>
-                        </router-link>
-                    </div>
-                    <hr>
-                </div>
+            <div class="post-preview pt-5">
+                <router-link to="/post/1">
+                    <h2 class="post-title"><strong>Chevrolet presenta la totalmente nueva Blazer</strong></h2>
+                    <h3 class="post-subtitle">Llega&nbsp;como la primera SUV deportiva de la marca</h3>
+                </router-link>
             </div>
+            <hr>
         </div>
         <footer>
             <div class="container">
